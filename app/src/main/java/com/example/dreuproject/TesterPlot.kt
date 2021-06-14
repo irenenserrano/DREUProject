d4abad6e961245bd3c39e13f8d755587dadbda03
iserrano <iserrano@mills.edu>
2021-06-14 14:07:11 -0700

    Rename file

--- app/src/main/java/com/example/dreuproject/TesterPlot.kt
+++ app/src/main/java/com/example/dreuproject/TesterPlot.kt
@@ -11,7 +11,7 @@ import java.text.ParsePosition
 import java.util.*
 
 
-class Tester : Activity() {
+class TesterPlot : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
