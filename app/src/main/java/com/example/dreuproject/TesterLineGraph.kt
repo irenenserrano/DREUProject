d603a0a3474723237b149f52b7302dd02360999d
Irene <iserrano@mill.edu>
2021-06-17 15:53:54 -0700

    Debuging

--- app/src/main/java/com/example/dreuproject/TesterLineGraph.kt
+++ app/src/main/java/com/example/dreuproject/TesterLineGraph.kt
@@ -3,6 +3,7 @@ package com.example.dreuproject
 import android.app.Activity
 import android.os.Bundle
 import android.os.DropBoxManager
+import android.util.Log
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
@@ -18,23 +19,30 @@ class TesterLineGraph: Activity() {
         setContentView(R.layout.tester_line_chart)
 
         // create a new data series
-        lateinit var series: LineGraphSeries<DataPoint>
+        var series1 = LineGraphSeries<DataPoint>()
+        var series2 = LineGraphSeries<DataPoint>()
         // link activity to layout view
         val graph: GraphView = findViewById(R.id.line_chart)
 
         // try to read and save data into series
         val minput = InputStreamReader(assets.open("tester_data_CSV.csv"))
         val reader = BufferedReader(minput)
-
-        var line: String
+        var line: String?
         var x: Double
-        var y: Double
+        var y1: Double
+        var y2: Double
         while (reader.readLine().also { line = it } != null) {
-            val row: List<String> = line.split(",")
+            val row: List<String> = line!!.split(",")
+            Log.i("Row", row.toString())
             x = row[0].toDouble()
-            y = row[1].toDouble()
-            series.appendData(DataPoint(x,y),true,  100)
+            y1 = row[1].toDouble()
+            y2 = row[2].toDouble()
+            // add data to series, unsure what the last element is for
+            series1.appendData(DataPoint(x,y1),true,  100)
+            series2.appendData(DataPoint(x,y2), true, 100)
         }
-        graph.addSeries(series)
+        // add series to graph
+        graph.addSeries(series1)
+        graph.addSeries(series2)
     }
 }
