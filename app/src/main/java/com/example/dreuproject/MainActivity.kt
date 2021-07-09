5d8afe647aeaec596764a48bdd95bac15b226eb9
Irene <iserrano@mill.edu>
2021-07-09 13:29:36 -0700

    saving work

--- app/src/main/java/com/example/dreuproject/MainActivity.kt
+++ app/src/main/java/com/example/dreuproject/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.AutoTransition
 import android.transition.TransitionManager
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         headerOne.setOnClickListener {
             // If the CardView is already expanded, set its visibility to gone
             // If it is not expanded, set its visibility to visible
+            Log.d("testing", "testing")
             if (hiddenView.visibility == View.VISIBLE) {
                 TransitionManager.beginDelayedTransition(cardView, AutoTransition())
                 hiddenView.visibility = View.GONE
